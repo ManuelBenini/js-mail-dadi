e979807be0e542a841ed9a219d555c160e02c199
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-11 19:00:16 +0200

    Primo commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+
+  MAIL
+  a. Chiedi all'utente la sua email,
+  b. Controlla se sia nella lista di chi può accedere (Creare   una lista con gli utenti autorizzati),
+  c. Stampa messaggio appropriato sull'esito del controllo.
+
+  DADI
+  a2. Generare un numero random da 1 a 6 per utente e PC,
+  b2. Stabilire il vincitore in base al punteggio più alto.
+
+
+
+  MAIL
+  1. Dichiarare variabili (Email ed array)
+  2. Prompt variabile Email
+  3. Creare ciclo For per controlli
+
+  DADI
+  1,2. Dichiarare variabili (Numero casuale per utente e PC)
+  2,2. Stabilire il numero più alto con messaggio di vincita
+*/
